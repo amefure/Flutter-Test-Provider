17b2c307276e73144a8eac5d75291da49b44d5f3
amefure <shachi10969@gmail.com>
2025-04-20 22:26:14 +0900

    Providerを使用したパフォーマンス改善コードの実装

--- lib/reverpod_sample/counter_riverpod_view.dart
+++ lib/reverpod_sample/counter_riverpod_view.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// 状態管理（StateProvider を使用）
+/// 状態管理（StateProvider を使用）
 final counterProvider = StateProvider<int>((ref) => 0);
 
+/// カウンター機能UI
 class CounterRiverpodView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -28,24 +29,68 @@ class CounterRiverpodView extends ConsumerWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    ref.read(counterProvider.notifier).state--;
-                  },
-                  child: const Icon(Icons.remove),
-                ),
+                // デクリメントボタン
+                DecrementButton(),
                 const SizedBox(width: 20),
-                FloatingActionButton(
-                  onPressed: () {
-                    ref.read(counterProvider.notifier).state++;
-                  },
-                  child: const Icon(Icons.add),
-                ),
+                // インクリメントボタン
+                IncrementButton(),
               ],
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+/// 最大カウント数
+final int MAX_COUNT = 10;
+
+/// インクリメント可能かどうかを計算するプロバイダ
+final canIncrementProvider = Provider<bool>((ref) {
+  return ref.watch(counterProvider) != MAX_COUNT;
+});
+
+/// インクリメントボタン
+/// Providerを使用して[canIncrementProvider]に検知ロジックを切り離す
+class IncrementButton extends ConsumerWidget {
+  const IncrementButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    /// インクリメント可能かどうか
+    /// watchで常に変化を観測し、変化するたびに再描画
+    final canIncrement = ref.watch(canIncrementProvider);
+    /// インクリメント
+    void increment() {
+      ref.read(counterProvider.notifier).update((state) => state + 1);
+    }
+    /// ボタンUI
+    return ElevatedButton(
+      onPressed: canIncrement ? increment : null,
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+/// デクリメントボタン
+/// この実装ではパフォーマンスに問題あり
+class DecrementButton extends ConsumerWidget {
+  const DecrementButton({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    /// デクリメント可能かどうか
+    /// watchで常に変化を観測し、変化するたびに再描画
+    final canDecrement = ref.watch(counterProvider) > 0;
+    /// デクリメント
+    void decrement() {
+      ref.read(counterProvider.notifier).update((state) => state - 1);
+    }
+    /// ボタンUI
+    return ElevatedButton(
+      onPressed: canDecrement ? decrement : null,
+      child: const Icon(Icons.remove),
     );
   }
 }
